aebe3e9341648c96ed72f6a92e04b28e8e9cb4e0
jlima2016 <jlima2016@pingry.org>
2015-11-12 11:57:20 -0500

    Update Period.java

--- Period.java
+++ Period.java
@@ -1,24 +1,44 @@
 import java.util.ArrayList;
-
+ 
 public class Period {
 	
 	ArrayList<Student> students;
 	Teacher teacher;
 	String name;
 	
-	public Period(ArrayList<Student> s, Teacher t, String n) {
-		students = s;
-		teacher = t;
+	//I currently need this for creating a seeded schedule
+	ArrayList<Teacher> possibleTeachers;
+	
+	//year-long, semester, trimester
+	String time;
+	
+	//does the class take flex (Y or N)
+	String flex;
+	
+	//changes during mutation
+	int slot;
+	
+	//constructor
+	//takes in name, time, flex (constant things) and slot
+	public Period(String n, String t, String f, int s, ArrayList<Teacher> pt) {
 		name = n;
+		time = t;
+		flex = f;
+		slot = s;
+		possibleTeachers = pt;
+		students = new ArrayList<Student>();
 	}
 	
-	public Period(String n) {
-		name = n;
-		students = new ArrayList<Student>();
+	public String getName() {
+		return name;
 	}
 	
 	public void addStudent(Student s) {
 		students.add(s);
+	}
+	
+	public Student removeStudent(int i) {
+		return students.remove(i);
 	}
 	
 	public void setTeacher(Teacher t) {
@@ -27,6 +47,22 @@ public class Period {
 	
 	public ArrayList<Student> getStudents() {
 		return students;
+	}
+	
+	public Teacher getTeacher() {
+		return teacher;
+	}
+	
+	public int numStudents() {
+		return students.size();
+	}
+	
+	public void setSlot(int s) {
+		slot = s;
+	}
+	
+	public int getSlot() {
+		return slot;
 	}
 	
 	public String toString() {
